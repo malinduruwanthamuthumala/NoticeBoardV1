7b8dd59791e634fef4e3c153655001fc7c20563d
malinduruwantha <malinduruwantha@gmail.com>
2023-06-01 14:38:57 +0530

    add notice list component

--- src/noticeboard/pages/LandingPage.js
+++ src/noticeboard/pages/LandingPage.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React,{useState}from "react";
 import NewNoticeForm from "../components/adddNewNotice/addNewNotice";
 import NoticeList from "../components/noticeList/noticeList";
 
 const LandingPage = (props)=>{
+
+    const [noticeList,setNoticeList] = useState([]);
+
+    const onNewNoticeAdd = (notice) =>{
+        setNoticeList((prevNotices)=>{
+            return [...prevNotices,notice];
+        }
+        );
+    }
+
     return(
-        <div className="containerfluid">
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-2">
+
+                </div>
+                <div className="col-md-7">           
+                    <NewNoticeForm onAddNewNotice={onNewNoticeAdd}></NewNoticeForm>
+                    <NoticeList noticeList={noticeList}></NoticeList>
+                    <NoticeListArchive></NoticeListArchive>
+                </div>
+                <div className="col-md-3">
+
+                </div>
+            </div>
             
-            <NewNoticeForm></NewNoticeForm>
-            <NoticeList></NoticeList>
         </div>
     )
 }
