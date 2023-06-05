a0cfc76166e178f34826a44a63689f5103bf5ca9
malinduruwantha <malinduruwantha@gmail.com>
2023-06-05 10:51:09 +0530

    react dom create portal feature

--- src/noticeboard/UI/errorModel.js
+++ src/noticeboard/UI/errorModel.js
@@ -8,38 +8,62 @@ import {
     MDBCardFooter,
     MDBBtn
   } from 'mdb-react-ui-kit';
-import "./errorModel.css"
-const ErrorModel = (props) => {
+import "./errorModel.css";
+import     ReactDOM  from "react-dom";
 
+const Backdrop = props =>{
+
+    
+    return (
+        <>
+        
+        <div className="backdrop"></div>
+        </>
+    );
+
+    
+}
+
+const Error = props => {
 
     const onModelClose = () => {
-        props.closeModel();
+        debugger
+        props.onClose();
     }
+
+    return (<div className="container-fluid row error_model">
+    <div className="col-md-3"></div>
+    <div className="col-md-6">
+    <MDBCard alignment='center'>
+        <MDBCardHeader>{props.title}</MDBCardHeader>
+        <MDBCardBody>
+            {
+                props.errors.map((error)=>{
+                  const [id,message] = error;
+                  return  <MDBCardText className="error_messages_model"> * {error}</MDBCardText>
+                })
+            }
+        
+        <MDBBtn href='#' onClick={onModelClose}>close</MDBBtn>
+        </MDBCardBody>
+    </MDBCard>
+    </div>
+    <div className="col-md-3"></div>
+</div>);
+}
+
+const ErrorModel = (props) => {   
+   
+
+debugger
     return(
-        <div>
-        <div className="container-fluid row error_model">
-            <div className="col-md-3"></div>
-            <div className="col-md-6">
-            <MDBCard alignment='center'>
-                <MDBCardHeader>{props.title}</MDBCardHeader>
-                <MDBCardBody>
-                    {
-                        props.errors.map((error)=>{
-                          const [id,message] = error;
-                          return  <MDBCardText className="error_messages_model"> * {error}</MDBCardText>
-                        })
-                    }
-                
-                <MDBBtn href='#' onClick={onModelClose}>close</MDBBtn>
-                </MDBCardBody>
-            </MDBCard>
-            </div>
-            <div className="col-md-3"></div>
-        </div>
-        <div className="backdrop"></div>
         
-        </div>
-        
+        <React.Fragment>
+            {ReactDOM.createPortal(<Backdrop/>,document.getElementById('backdrop-root'))}
+            {
+            
+            ReactDOM.createPortal(<Error onClose={props.closeModel}  errors={props.errors} title={props.title}/>,document.getElementById('overlay-root'))}
+        </React.Fragment>        
     )
 }
 
