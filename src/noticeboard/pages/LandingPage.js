3a442f129c75a25285de0bfe7e9f26cefe7e1300
malinduruwantha <malinduruwantha@gmail.com>
2023-06-09 16:38:19 +0530

    landin gpage

--- src/noticeboard/pages/LandingPage.js
+++ src/noticeboard/pages/LandingPage.js
@@ -4,16 +4,26 @@ import NoticeList from "../components/noticeList/noticeList";
 import ErrorModel from "../UI/errorModel";
 
 const LandingPage = (props)=>{
-
+    let listLocal = [];
     const [noticeList,setNoticeList] = useState([]);
     const [errors,setErrors] = useState([]);
     const [isModelHidden,setIsModelHidden] = useState(true);
     const onNewNoticeAdd = (notice) =>{
+        debugger
         setNoticeList((prevNotices)=>{
             return [...prevNotices,notice];
         }
         );
+        const fromStorage = localStorage.getItem("noticeList");
+        let listConverted = [];
+        if (isJSONCompatible(fromStorage)){
+            listConverted = JSON.parse(fromStorage);
+        }
+        listConverted.push(notice);
+        localStorage.setItem("noticeList",JSON.stringify(listConverted));
+   
     }
+    
 
     const handleFormErrors = (errors) => {
         errors.length>0 ? setIsModelHidden(true) : setIsModelHidden(false);
@@ -23,6 +33,25 @@ const LandingPage = (props)=>{
     const onModelClose = () => {
         setIsModelHidden(false);
     }
+
+    React.useState(()=>{
+        debugger
+        const noticeListfromstorage = localStorage.getItem("noticeList");
+        const allowIn = isJSONCompatible(noticeListfromstorage);
+        const jsonConvertedList = JSON.parse(noticeListfromstorage);
+        if(noticeList.length == 0 && allowIn && Array.isArray(jsonConvertedList) ){
+            setNoticeList(jsonConvertedList);
+        }
+    },[])
+
+    function isJSONCompatible(str) {
+        try {
+          JSON.parse(str);
+          return true;
+        } catch (error) {
+          return false;
+        }
+      }
 
     return(
         <React.Fragment>
